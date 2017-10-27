ac0ae3af3a70e8c296e83620636fb0b2abe1b60d
Jane Aguero <jane.m.aguero@gmail.com>
2017-10-27 19:21:12 -0400

    code fix to spotify

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 var keys = require("./keys.js");
 var Twitter = require("twitter");
-var Spotify = require("node-spotify-api");
+var spotifyAPI = require("node-spotify-api");
 var request = require("request");
 var fs = require("fs");
 
